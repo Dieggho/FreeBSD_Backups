0fb42f194b738d6a8b9ad30f6487f9e93e02d913
Diëggho <diegghooliver@gmail.com>
2026-08-08 01:22:12 -0300

    Add files via upload

--- home/user/.config/hypr/hyprland.lua
+++ home/user/.config/hypr/hyprland.lua
@@ -86,10 +86,10 @@ end)
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
     general = {
-        gaps_in  = 5,
+        gaps_in  = 7,
         gaps_out = 12,
 
-        border_size = 5,
+        border_size = 6,
 
         col = {
             active_border   = { colors = {"rgba(16181Dee)", "rgba(13151699)"}, angle = 99 },
@@ -296,13 +296,13 @@ hl.bind(mainMod .. "+ SHIFT + down",  hl.dsp.window.move({ direction = "down" })
 -- Move active window to a workspace with mainMod + SHIFT + [0-9]
 for i = 1, 10 do
     local key = i % 10 -- 10 maps to key 0
-    hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
-    hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
+    hl.bind("SUPER + " .. key,             hl.dsp.focus({ workspace = i}))
+    hl.bind("SUPER + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
 end
 
 -- Example special workspace (scratchpad)
-hl.bind(mainMod .. " + S",         hl.dsp.workspace.toggle_special("magic"))
-hl.bind(mainMod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }))
+hl.bind("SUPER + S",         hl.dsp.workspace.toggle_special("magic"))
+hl.bind("SUPER + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }))
 
 -- Scroll through existing workspaces with mainMod + scroll
 hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
@@ -462,11 +462,7 @@ hl.window_rule({
 
 hl.window_rule({
     name  = "Gimp-2.10",
-    match = { class = "Gimp-2.10",
-    float      = false,
-    fullscreen = false,
-    pin        = false,
-  },
+    match = { class = "Gimp-2.10"},
     workspace = 6,
 })
 
